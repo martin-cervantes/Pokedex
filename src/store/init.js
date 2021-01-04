fa88b87e74d22ad874b1ad59d63ddfdb5d006ed5
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-04 14:32:27 -0700

    Add pokemons types list

---
+++ src/store/init.js
@@ -0,0 +1,24 @@
+const types = [
+  'Bug',
+  'Dark',
+  'Dragon',
+  'Electric',
+  'Fairy',
+  'Fighting',
+  'Fire',
+  'Flying',
+  'Ghost',
+  'Grass',
+  'Ground',
+  'Ice',
+  'Normal',
+  'Poison',
+  'Psychic',
+  'Rock',
+  'Steel',
+  'Water'
+];
+
+const pokemons = [];
+
+export { pokemons, types };
