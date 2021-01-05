46f232dbaa570c4a5ce660a604b9b8fd6dc0d252
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-05 14:34:02 -0700

    Add async method to retrieve information

--- src/store/init.js
+++ src/store/init.js
@@ -1,3 +1,18 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+
+const getData = async (num) => {
+  try {
+    const response = await fetch(`${url}${num}`, { mode: 'cors' });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    alert(error);
+  }
+  return false;
+};
+
 const types = [
   'Bug',
   'Dark',
