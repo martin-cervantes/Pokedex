c2f20b74475b18326b89288815f6385181adb83f
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-07 12:14:27 -0700

    Change duplicated name acording to action name

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -3,7 +3,7 @@ import { CHANGE_FILTER } from '../actions';
 const filterReducer = (state = 'All', action) => {
   switch (action.type) {
     case CHANGE_FILTER:
-      return action.type;
+      return action.pokemon_type;
     default:
       return state;
   }
