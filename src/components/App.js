7909feeef12e8bfcea27db1004ca554f4d3a44ef
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-08 14:27:54 -0700

    Add app component with header and pokemons list inside

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../containers/Header';
+// import PokemonsList from '../containers/PokemonsList';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>PokemonsList</h1>
+    </div>
+  );
+}
+
+export default App;
