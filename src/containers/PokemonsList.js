db45ca86d8a4dadd4a057a9b850263cfc95b2acb
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-12 06:57:15 -0700

    Add typechecking with proptypes

--- src/containers/PokemonsList.js
+++ src/containers/PokemonsList.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Pokemon from '../components/Pokemon';
@@ -11,29 +10,8 @@ const PokemonsList = ({ pokemons }) => (
   </div>
 );
 
-Pokemon.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    types: PropTypes.arrayOf(
-      PropTypes.shape({
-        slot: PropTypes.number.isRequired,
-        name: PropTypes.exact({
-          name: PropTypes.string,
-          url: PropTypes.string,
-        }),
-      }),
-    ),
-    weight: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    stats: PropTypes.arrayOf(
-      PropTypes.shape({
-        base_stat: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-      }),
-    ),
-    sprites: PropTypes.string.isRequired,
-  }).isRequired,
+PokemonsList.propTypes = {
+  pokemons: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 const mapStateToProps = state => ({
