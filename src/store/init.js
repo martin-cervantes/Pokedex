ccb3dfe6af301c1c9b3625a97fd36f0bb02280d7
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-13 09:31:33 -0700

    Remove function and change fetch using promises

--- src/store/init.js
+++ src/store/init.js
@@ -2,30 +2,23 @@ const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 const pokemons = [];
 
-const getPokemon = async (num) => {
-  try {
-    const response = await fetch(`${url}${num}`, { mode: 'cors' });
-
-    const data = await response.json();
-
-    pokemons.push({
-      id: data.id,
-      name: data.name,
-      types: data.types.map(t => (t.type.name)),
-      weight: data.weight,
-      height: data.height,
-      stats: data.stats.map(s => ({ name: s.stat.name, base: s.base_stat })),
-      abilities: data.abilities.map(a => a.ability.name),
-      moves: data.moves.map(m => m.move.name),
-      sprite: data.sprites.other.dream_world.front_default,
+for (let num = 1; num <= 3; num += 1) {
+  fetch(`${url}${num}`, { mode: 'cors' })
+    .then(response => response.json())
+    .then(data => {
+      pokemons.push({
+        id: data.id,
+        name: data.name,
+        types: data.types.map(t => (t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))),
+        weight: data.weight,
+        height: data.height,
+        stats: data.stats.map(s => ({ name: s.stat.name, base: s.base_stat })),
+        abilities: data.abilities.map(a => a.ability.name),
+        moves: data.moves.map(m => m.move.name),
+        sprite: data.sprites.other.dream_world.front_default,
+      });
     });
-
-    return data;
-  } catch (error) {
-    alert(error);
-  }
-  return false;
-};
+}
 
 const types = [
   'Bug',
@@ -47,9 +40,5 @@ const types = [
   'Steel',
   'Water',
 ];
-
-for (let i = 1; i <= 3; i += 1) {
-  getPokemon(i);
-}
 
 export { pokemons, types };
