e95e4511a2ef7eb7e1387c8820410956166686b7
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-13 09:32:24 -0700

    Add filter to render pokemons

--- src/containers/PokemonsList.js
+++ src/containers/PokemonsList.js
@@ -2,16 +2,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Pokemon from '../components/Pokemon';
 
-const PokemonsList = ({ pokemons }) => (
+const PokemonsList = ({ pokemons, filter }) => (
   <div className="pokemons">
     {
-      pokemons.map(pokemon => <Pokemon key={pokemon.id} data={pokemon} />)
+      pokemons
+        .filter(pokemon => (filter === 'All' ? true : pokemon.types.some(type => type === filter)))
+        .map(pokemon => <Pokemon key={pokemon.id} data={pokemon} />)
     }
   </div>
 );
 
 PokemonsList.propTypes = {
   pokemons: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
