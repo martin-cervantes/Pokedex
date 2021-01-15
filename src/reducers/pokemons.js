23fa5b14b39de783747f632f87b146eec676518b
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-15 15:08:24 -0700

    Change initial state for pokemons reducer

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,22 +1,9 @@
 import { ADD_POKEMON } from '../actions';
-import { pokemons } from '../store/init';
 
-const pokemonsReducer = (state = pokemons, action) => {
+const pokemonsReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_POKEMON:
-      return [...state,
-        {
-          id: action.pokemon.id,
-          name: action.pokemon.name,
-          types: action.pokemon.types,
-          weight: action.pokemon.weight,
-          height: action.pokemon.height,
-          stats: action.pokemon.stats,
-          abilities: action.pokemon.abilities,
-          moves: action.pokemon.moves,
-          sprites: action.pokemon.sprites,
-        },
-      ];
+      return [...state, action.pokemon];
     default:
       return state;
   }
