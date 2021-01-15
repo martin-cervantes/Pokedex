3090a3edcca32878e7053ec00cb8d1d90ac36444
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-15 15:07:19 -0700

    Change types as export default variable

--- src/store/init.js
+++ src/store/init.js
@@ -1,25 +1,3 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-
-const pokemons = [];
-
-for (let num = 1; num <= 3; num += 1) {
-  fetch(`${url}${num}`, { mode: 'cors' })
-    .then(response => response.json())
-    .then(data => {
-      pokemons.push({
-        id: data.id,
-        name: data.name,
-        types: data.types.map(t => (t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))),
-        weight: data.weight,
-        height: data.height,
-        stats: data.stats.map(s => ({ name: s.stat.name, base: s.base_stat })),
-        abilities: data.abilities.map(a => a.ability.name),
-        moves: data.moves.map(m => m.move.name),
-        sprite: data.sprites.other.dream_world.front_default,
-      });
-    });
-}
-
 const types = [
   'Bug',
   'Dark',
@@ -41,4 +19,4 @@ const types = [
   'Water',
 ];
 
-export { pokemons, types };
+export default types;
