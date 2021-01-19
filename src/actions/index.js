a960c76d24af17fb452a47f1bcd7798e5dfe2bb3
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-18 17:54:21 -0700

    Add items when loading page for first time updating the state

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,24 @@ export const changeFilter = pokemonType => ({
   type: CHANGE_FILTER,
   pokemonType,
 });
+
+export function fetchData(url) {
+  return (dispatch) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch(addPokemon({
+        id: data.id,
+        name: data.name,
+        types: data.types.map(t => {
+          const { name } = t.type;
+          return name.charAt(0).toUpperCase() + name.slice(1);
+        }),
+        weight: data.weight,
+        height: data.height,
+        stats: data.stats.map(s => ({ name: s.stat.name, base: s.base_stat })),
+        abilities: data.abilities.map(a => a.ability.name),
+        moves: data.moves.map(m => m.move.name),
+        sprite: data.sprites.other.dream_world.front_default,
+      })));
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../containers/Header';
 import PokemonsList from '../containers/PokemonsList';
+import { fetchData } from '../actions';
 
-const App = () => (
-  <div>
-    <Header />
-    <PokemonsList />
-  </div>
-);
+const url = 'https://pokeapi.co/api/v2/pokemon/';
 
-export default App;
+const App = props => {
+  useEffect(() => {
+    for (let num = 1; num <= 10; num += 1) {
+      props.fetchData(`${url}${num}`);
+    }
+  });
+
+  return (
+    <div>
+      <Header />
+      <PokemonsList />
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchData: url => dispatch(fetchData(url)),
+});
+
+App.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(App);
