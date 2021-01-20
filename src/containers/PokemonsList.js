409b99af34c71bda1a77328a65475d1adfacadd7
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-20 09:20:22 -0700

    Add change filter to render in pokemon list

--- src/containers/PokemonsList.js
+++ src/containers/PokemonsList.js
@@ -1,16 +1,49 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import TypeFilter from '../components/TypeFilter';
 import Pokemon from '../components/Pokemon';
+import { changeFilter } from '../actions';
 
-const PokemonsList = ({ pokemons, filter }) => (
-  <div className="pokemons">
-    {
-      pokemons
-        .filter(pokemon => (filter === 'All' ? true : pokemon.types.some(type => type === filter)))
-        .map(pokemon => <Pokemon key={pokemon.id} data={pokemon} />)
-    }
-  </div>
-);
+class PokemonsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+  }
+
+  handleFilterChange(event) {
+    event.preventDefault();
+
+    const { changeFilter } = this.props;
+
+    changeFilter(event.target.value);
+  }
+
+  render() {
+    const { pokemons, filter } = this.props;
+
+    return (
+      <div id="pokemons">
+        <TypeFilter handleFilterChange={this.handleFilterChange} />
+        {
+          pokemons
+            .sort((a, b) => {
+              if (a.id > b.id) {
+                return 1;
+              }
+              if (a.id < b.id) {
+                return -1;
+              }
+              return 0;
+            })
+            .filter(pokemon => (filter === 'All' ? true : pokemon.types.some(type => type === filter)))
+            .map(pokemon => <Pokemon key={pokemon.id} data={pokemon} />)
+        }
+      </div>
+    );
+  }
+}
 
 PokemonsList.propTypes = {
   pokemons: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -22,4 +55,10 @@ const mapStateToProps = state => ({
   filter: state.filter,
 });
 
-export default connect(mapStateToProps, null)(PokemonsList);
+const mapDispatchToProps = dispatch => ({
+  changeFilter: filter => {
+    dispatch(changeFilter(filter));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonsList);
