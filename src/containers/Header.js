804d0bd1c41450b7d617e2b018288288f947a01e
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-20 09:19:22 -0700

    Move change filter from header to pokemon list

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,44 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import TypeFilter from '../components/TypeFilter';
-import { changeFilter } from '../actions';
-
 import logo from '../assets/img/logo.png';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
+const Header = () => (
+  <header>
+    <img src={logo} id="logo" alt="logo" />
+  </header>
+);
 
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-  }
-
-  handleFilterChange(event) {
-    event.preventDefault();
-
-    const { changeFilter } = this.props;
-
-    changeFilter(event.target.value);
-  }
-
-  render() {
-    return (
-      <header>
-        <img src={logo} className="brand" alt="logo" />
-        <TypeFilter handleFilterChange={this.handleFilterChange} />
-      </header>
-    );
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-  changeFilter: filter => {
-    dispatch(changeFilter(filter));
-  },
-});
-
-Header.propTypes = {
-  changeFilter: PropTypes.func.isRequired,
-};
-
-export default connect(null, mapDispatchToProps)(Header);
+export default Header;
