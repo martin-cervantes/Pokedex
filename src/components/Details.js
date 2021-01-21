a7be4e05b5eeb5efe90011996a10835dd7607e5f
Martin Cervantes <cervantes.martine@gmail.com>
2021-01-21 09:26:38 -0700

    Add Pokémon details page to display all information for the selected Pokémon

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,22 +1,85 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 const Details = ({ match, pokemons }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   const { id } = match.params;
   const pokemon = pokemons.find(p => p.id === parseInt(id, 10));
 
+  const {
+    name,
+    types,
+    weight,
+    height,
+    stats,
+    abilities,
+    moves,
+    sprite,
+  } = pokemon;
+
   return (
-    <div>
-      <h1>Pokemon Details</h1>
-      <div>{id}</div>
-      <div>name: {pokemon.name}</div>
-      <Link to="/">Back</Link>
+    <div className="pokemon_details">
+      <div className="section1">
+        <div className="pokemon_container">
+          <img className="pokemon_img" src={sprite} alt={name} />
+
+          <p className="pokemon_name">{name}</p>
+
+          <div className="pokemon_num">{id}</div>
+        </div>
+
+        <div className="pokemon_stats">
+          <div className="group">
+            <p><strong>Type:</strong></p>
+            <p><strong>Weight:</strong></p>
+            <p><strong>Height:</strong></p>
+            <p className="label"><strong>HP:</strong></p>
+            <p className="label"><strong>Attack:</strong></p>
+            <p className="label"><strong>Defense:</strong></p>
+            <p className="label"><strong>Special-attack:</strong></p>
+            <p className="label"><strong>Special-defense:</strong></p>
+            <p className="label"><strong>Speed:</strong></p>
+          </div>
+
+          <div className="group">
+            <div className="left type">
+              { types.map(t => <div className={`${t} capsule`}>{t}</div>) }
+            </div>
+            <p className="left">{weight}</p>
+            <p className="left">{height}</p>
+            { stats.map(s => <div className="progressbar"><div style={{ width: s.base }}></div></div>) }
+          </div>
+        </div>
+      </div>
+
+      <div className="section2">
+        <strong className="underline">Abilities:</strong>
+        <ul>
+          { abilities.map(a => <li>{a}</li>) }
+        </ul>
+      </div>
+
+      <div className="section2">
+        <strong className="underline">Moves:</strong>
+        <ul>
+          { moves.map(m => <li>{m}</li>) }
+        </ul>
+      </div>
+
+      <div className="button"><Link to='/' className="button_link" href="">Back</Link></div>
     </div>
   );
 };
 
 Details.propTypes = {
+  match: PropTypes.object.isRequired,
   pokemons: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
