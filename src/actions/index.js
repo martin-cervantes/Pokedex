ca1cf79b7144bd8073776b8112d9123ab52f76f4
mcervantes71 <cervantes.martine@gmail.com>
2021-02-22 13:07:47 -0700

    Add action and change name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const ADD_POKEMONS = 'ADD_POKEMONS';
+export const ADD_POKEMON_INFO = 'ADD_POKEMON_INFO';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const addPokemons = pokemons => ({
@@ -6,15 +7,28 @@ export const addPokemons = pokemons => ({
   pokemons,
 });
 
+export const addPokemonInfo = pokemonInfo => ({
+  type: ADD_POKEMON_INFO,
+  pokemonInfo,
+});
+
 export const changeFilter = pokemonType => ({
   type: CHANGE_FILTER,
   pokemonType,
 });
 
-export const fetchData = count => dispatch => {
+export const fetchPokemons = count => dispatch => {
   const url = 'https://pokemons-api-project.herokuapp.com/pokemons/';
 
   fetch(`${url}${count}`)
     .then(response => response.json())
     .then(data => dispatch(addPokemons(data)));
+};
+
+export const fetchPokemonInfo = id => dispatch => {
+  const url = 'https://pokemons-api-project.herokuapp.com/pokemon_info/';
+
+  fetch(`${url}${id}`)
+    .then(response => response.json())
+    .then(data => dispatch(addPokemonInfo(data)));
 };
